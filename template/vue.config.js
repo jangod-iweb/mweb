5abafbbd258acb210f95eb29f35f22e72d6f99f6
ZGL <32898567+zhangguanlei@users.noreply.github.com>
2024-02-17 16:00:06 +0800

    Update vue.config.js
    打包模式调整
    优化启动速度

--- template/vue.config.js
+++ template/vue.config.js
@@ -3,12 +3,17 @@ const RuntimePublicPathPlugin = require('webpack-runtime-public-path-plugin');
 const fileManagerPlugin = require('filemanager-webpack-plugin');
 const GenerateConfig = require('@jangod/iweb-sdk/generate-config.js');
 const Config = GenerateConfig();
+const package = require('./package.json')
 function resolve(dir) {
     return path.join(__dirname,dir)
 }
+const CDN = {
+    js:[],
+    css:[]
+}
 module.exports = {
     publicPath: Config.context,
-    outputDir: "dist",
+    outputDir: `target/${Config.version}/${package.name}`,
     assetsDir: "static",
     runtimeCompiler: false,
     productionSourceMap: false,
@@ -24,6 +29,12 @@ module.exports = {
             }
         }
     },
+    configureWebpack: {
+        cache:{
+            type: 'filesystem',
+            allowCollectingMemory: true
+        }
+    }
     chainWebpack: config =>{
         config.resolve.alias
             .set('@_src',resolve('src'));
@@ -31,6 +42,15 @@ module.exports = {
             .use(RuntimePublicPathPlugin,[{
                 runtimePublicPath: '__iweb_public_path || __iweb_context_path'
             }]);
+        config.plugin('define').tap(args => {
+            args[0]['process.env'].versionTimestamp = JSON.stringify(new Date().getTime())
+            args[0]['process.env'].cdn = JSON.stringify(CDN)
+            return args
+        });
+        config.plugin('webpackBar').use(WebpackBar).tap(args => {
+            return [...args, {
+                color: 'green', name: "编译中"
+            }]});
         config.plugin('html')
             .tap(options => {
                 options[0].inject = false;
@@ -41,8 +61,8 @@ module.exports = {
                 .use(fileManagerPlugin, [{
                     events:{
                         onEnd: {
-                            delete: [`./dist_*\.zip`,'./public/version.json'],
-                            archive: [{source: './dist', destination: `./target/dist_${Config.version}.zip`}]
+                            delete: [`./${package.name}_*\.zip`,'./public/version.json'],
+                            archive: [{source: `./target/${Config.version}`, destination: `./target/${package.name}_${Config.version}.zip`}]
                         }
                     }
                 }])
